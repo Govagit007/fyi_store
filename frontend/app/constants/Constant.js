b14f9f4a85280d35678e80db45fce04e61790f25
AKKAMGARI VENU <bt19cse013@nituk.ac.in>
2024-08-18 02:19:03 +0530

    sdfef

--- frontend/app/constants/Constant.js
+++ frontend/app/constants/Constant.js
@@ -1,1 +1,2 @@
-export const BACKEND_SERVER = "http://localhost:5100/api/";
+// export const BACKEND_SERVER = "http://localhost:5100/api/";
+export const BACKEND_SERVER = "https://fyi-store-1.onrender.com/api/";
